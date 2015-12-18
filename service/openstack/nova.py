aaa5b1b6cd4ed8082ed6a9a14418cd1678c01528
luoyancn <luoyan_cn@outlook.com>
2015-12-18 09:54:09 +0800

    Check if the project_id equals urls
    Signed-off-by: luoyancn <luoyan_cn@outlook.com>

--- service/openstack/nova.py
+++ service/openstack/nova.py
@@ -1,5 +1,6 @@
 import json
 from flask import Blueprint
+from werkzeug.exceptions import BadRequest
 
 import request as httprequest
 from .keystone import commonfun
@@ -10,10 +11,26 @@ from flask import request
 novamod = Blueprint('novamod', __name__)
 
 
+def _check_nova_project(project_id, auth):
+    check_project(project_id)
+    if project_id not in auth[1][0]:
+        msg = 'You try to visit the %s but your project_id is %s' %\
+            (auth[1][0], project_id)
+        raise BadRequest(description=msg)
+
+
+def _check_cinder_project(project_id, auth):
+    check_project(project_id)
+    if project_id not in auth[3][0]:
+        msg = 'You try to visit the %s but your project_id is %s' %\
+            (auth[3][0], project_id)
+        raise BadRequest(description=msg)
+
+
 @novamod.route('/v2.1/<project_id>/servers/<server_id>', methods=['GET'])
 @commonfun
 def get_server(auth, region, project_id, server_id):
-    check_project(project_id)
+    _check_nova_project(project_id, auth)
     kwargs = {'headers': {'X-Openstack-Region': region}}
     resp = httprequest.httpclient(
         'GET', auth[1][0] + '/servers/%s' % server_id,
@@ -24,7 +41,7 @@ def get_server(auth, region, project_id, server_id):
 @novamod.route('/v2.1/<project_id>/servers/detail', methods=['GET'])
 @commonfun
 def list_servers(auth, region, project_id):
-    check_project(project_id)
+    _check_nova_project(project_id, auth)
     kwargs = {'headers': {'X-Openstack-Region': region}}
     kwargs['params'] = request.args
     resp = httprequest.httpclient(
@@ -36,7 +53,7 @@ def list_servers(auth, region, project_id):
 @novamod.route('/v2.1/<project_id>/flavors/detail', methods=['GET'])
 @commonfun
 def list_flavors(auth, region, project_id):
-    check_project(project_id)
+    _check_nova_project(project_id, auth)
     kwargs = {'headers': {'X-Openstack-Region': region}}
     resp = httprequest.httpclient(
         'GET', auth[1][0] + '/flavors/detail',
@@ -47,7 +64,7 @@ def list_flavors(auth, region, project_id):
 @novamod.route('/v2/<project_id>/volumes/<volume_id>', methods=['GET'])
 @commonfun
 def get_volume(auth, region, project_id, volume_id):
-    check_project(project_id)
+    _check_cinder_project(project_id, auth)
     kwargs = {'headers': {'X-Openstack-Region': region}}
     kwargs['params'] = request.args
     resp = httprequest.httpclient(
@@ -59,7 +76,7 @@ def get_volume(auth, region, project_id, volume_id):
 @novamod.route('/v2/<project_id>/volumes/detail', methods=['GET'])
 @commonfun
 def list_volumes(auth, region, project_id):
-    check_project(project_id)
+    _check_cinder_project(project_id, auth)
     kwargs = {'headers': {'X-Openstack-Region': region}}
     resp = httprequest.httpclient(
         'GET', auth[3][0] + '/volumes/detail',
@@ -70,7 +87,7 @@ def list_volumes(auth, region, project_id):
 @novamod.route('/v2/<project_id>/snapshots/detail', methods=['GET'])
 @commonfun
 def list_snapshots(auth, region, project_id):
-    check_project(project_id)
+    _check_cinder_project(project_id, auth)
     kwargs = {'headers': {'X-Openstack-Region': region}}
     resp = httprequest.httpclient(
         'GET', auth[3][0] + '/snapshots/detail',
